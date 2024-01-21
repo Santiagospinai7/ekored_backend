ec13d9d88e2d7d08609e6b538213060c006628ff
Santiagospinai7 <santiagospidrobo15@gmail.com>
2024-01-21 18:36:54 -0500

    conexion de la base de datos y aplicaciones creadas

--- ekored/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class EkoredConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ekored'
--- ekored_backend/urls.py
+++ ekored_backend/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from recepcion import views as recepcionViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('recepcion/', recepcionViews.home)
 ]
